a4b6aec53d100d4e4842975d09fb1dced16ef511
Muhammed Shaheer <ckshaheer2012@gmail.com>
2021-03-29 15:10:32 +0530

    basic setup

---
+++ src/main/java/com/github/muhammedshaheer/springapachekafka/controller/KafkaController.java
@@ -0,0 +1,8 @@
+package com.github.muhammedshaheer.springapachekafka.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+}
