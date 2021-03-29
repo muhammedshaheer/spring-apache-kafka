3bce5dc0af7225aeacbabc89ed2195bc989ae9b8
Muhammed Shaheer <ckshaheer2012@gmail.com>
2021-03-29 15:53:45 +0530

    kafka producer implemented

--- src/main/java/com/github/muhammedshaheer/springapachekafka/controller/KafkaController.java
+++ src/main/java/com/github/muhammedshaheer/springapachekafka/controller/KafkaController.java
@@ -1,8 +1,24 @@
 package com.github.muhammedshaheer.springapachekafka.controller;
 
+import com.github.muhammedshaheer.springapachekafka.model.KafkaModel;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/kafka")
 public class KafkaController {
 
+    private final KafkaTemplate<String, KafkaModel> kafkaTemplate;
+
+    public KafkaController(KafkaTemplate<String, KafkaModel> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping("")
+    public void post(@RequestBody KafkaModel kafkaModel) {
+        kafkaTemplate.send("MY_TOPIC", kafkaModel);
+    }
 }
---
+++ src/main/java/com/github/muhammedshaheer/springapachekafka/model/KafkaModel.java
@@ -0,0 +1,16 @@
+package com.github.muhammedshaheer.springapachekafka.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class KafkaModel {
+
+    private String field1;
+    private String field2;
+}
